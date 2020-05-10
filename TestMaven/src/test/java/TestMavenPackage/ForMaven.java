5b4bb63254bb295c4e544850c2417e4cd3433510
sumanmitra19 <mitr.suman99@gmail.com>
2020-05-11 02:52:07 +0530

    Eighth

--- TestMaven/src/test/java/TestMavenPackage/ForMaven.java
+++ TestMaven/src/test/java/TestMavenPackage/ForMaven.java
@@ -9,12 +9,12 @@ public class ForMaven {
 	@Test
 	public void PageTest()
 	{
-		System.out.println("Welcome to the World of All......");
+		System.out.println("Welcome to the World of All.....");
 		
 		System.setProperty("webdriver.chrome.driver", "E:\\workspace\\Selenium_Practice\\Chrome_Driver\\chromedriver.exe");
 		WebDriver dr = new ChromeDriver();
 		dr.get("https://www.flipkart.com");
 		System.out.println("Page Title is : " + dr.getTitle());
-		System.out.println("Test Case finally passed and I'm ---->>>>> SUPER HAPPY man.hello>>>>>...||..");
+		System.out.println("Test Case finally passed and I'm ---->>>>> SUPER HAPPY man.hello>>>>>.....");
 	}
 }
